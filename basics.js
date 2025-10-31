7c0e9d0b07860fa6f50c1bdff10fb5f004cc495d
damonliftz <aidendamon16@gmail.com>
2025-10-31 09:02:12 +0200

    exercise 1

---
+++ basics.js
@@ -0,0 +1,7 @@
+const firstname = "Aiden";
+const lastname = "Damon";
+let age = 24;
+const fullname = firstname + " " + lastname;
+console.log(" Hello, my name is " + fullname + " and i am " + age + " years old ");
+age = 23;
+console.log(" Last year I was " + age + " years old. ");
